ee4577f21b0bf45412f5206d3fcaf95c864e3613
Denys Daneliuk <d.danelyuk@gmail.com>
2024-08-15 23:52:14 +0200

    update form

--- client/components/form/CreateFormBtn.tsx
+++ client/components/form/CreateFormBtn.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, {useForm} from 'react';
+import React from 'react';
 import {
   Dialog,
   DialogContent,
@@ -10,7 +10,7 @@ import {
   DialogTitle,
   DialogTrigger
 } from '@/components/ui/dialog';
-import { Button } from '@/components/ui/button';
+import { useForm } from 'react-hook-form';
 import {
   Form,
   FormControl,
@@ -20,15 +20,20 @@ import {
   FormLabel,
   FormMessage
 } from '@/components/ui/form'
-import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ImSpinner2 } from "react-icons/im";
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { toast } from '@/components/ui/use-toast';
+import { Textarea } from '@/components/ui/textarea';
 
 const formShema = z.object({
   name: z.string().min(4),
   description: z.string().optional(),
 });
 
-type formSchemaType = z.infer<typeof formShema>
+type formSchemaType = z.infer<typeof formShema>;
 
 function CreateFormBtn(){
   const form = useForm<formSchemaType>({
@@ -36,7 +41,15 @@ function CreateFormBtn(){
   });
 
   function onSubmit(values: formSchemaType){
-    console.log('values', values)
+    try {
+      
+    } catch (error) {
+      toast({
+        title: 'Error',
+        description: 'Something went wrong',
+        variant: 'destructive'
+      })
+    }
   }
   return (
     <Dialog>
@@ -51,8 +64,44 @@ function CreateFormBtn(){
           </DialogDescription>
         </DialogHeader>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-2'></form>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-2'>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+               <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+               </FormItem> 
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+               <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea rows={5} {...field} />
+                </FormControl>
+               </FormItem> 
+              )}
+            />
+          </form>
         </Form>
+        <DialogFooter>
+          <Button 
+            onClick={() => {
+              console.log('fsd')
+              form.handleSubmit(onSubmit)
+            }}
+            disabled={form.formState.isSubmitting} className='w-full mt-4'>
+            { !form.formState.isSubmitting && <span>Save</span> }
+            { form.formState.isSubmitting && <ImSpinner2 className='animate-spin'/> }
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   )
